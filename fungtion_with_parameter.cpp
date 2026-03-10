5d5e60833e1f0590cb421560af990bee4d860882
Riski153 <m.riski.f21@mail.umy.ac.id>
2026-03-10 11:36:08 +0700

    update code

--- fungtion_with_parameter.cpp
+++ fungtion_with_parameter.cpp
@@ -25,3 +25,10 @@ void output()
 {
     cout << "Luasnya : " << luasPersegi(panjang, lebar) << endl;
 }
+
+int mian()
+{
+    input();
+    output();
+    cout << "Kelilingnya : " << kelilingPersegi(panjang, lebar);
+}
